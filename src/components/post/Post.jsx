d450133b0aabf86efe395fd06f762c91bcbe506c
HKAgrwl <harshkumaragarwal2003@gmail.com>
2022-08-05 18:02:03 +0530

    5-aug

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './post.css'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {Users} from '../../dummyData'
+import { useState } from 'react';
+
+export default function Post({post}) {
+    const [like,setLike] = useState(post.like)
+    const [isLiked,setIsLiked] = useState(false)
+
+    const likeHandler=()=>{
+        setLike(isLiked? like-1:like+1)
+        setIsLiked(isLiked? false:true)
+    }
+
+  return (
+    <div className='post'>
+        <div className="postWrapper">
+            <div className="postTop">
+                <div className="postTopLeft">
+                    <img className='postProfileImg' src={Users.find((u)=> u.id===post.userId).profilePicture}alt="" />
+                    <div className="postUsername">{Users.filter((u)=> u.id===post.userId)[0].username}</div>
+                    <div className="postDate">{post.date}</div>
+                </div>
+                <div className="postTopRight"></div>
+                <MoreVertIcon/>
+            </div>
+            <div className="postCenter">
+                <span className="postText">{post?.desc}</span>
+                <img className='postImg' src={post.photo} alt="" />
+            </div>
+            <div className="postBottom">
+                <div className="postBottomLeft">
+                    <img className='likeIcon' src="/assets/like.png" alt="" onClick={likeHandler} />
+                    <img className='likeIcon' src="/assets/heart.png" alt="" />
+                    <span className="postLikeCounter">{like} likes</span>
+                </div>
+                <div className="postBottomRight">
+                    <span className="postCommentText">{post.comment} comments</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
